4890ef489aa5ea7b0cdb6fe7237364c035267e86
Pedro Almeida <pedro_almeida1191@hotmail.com>
2023-08-19 17:55:20 +0100

    refactor project rendering

---
+++ src/js/project-details.js
@@ -0,0 +1,174 @@
+export const projectDetails = [
+  {
+    thumbnail: '/img/projects/sesmt/sesmt-home.webp',
+    title: 'SESMT Digital',
+    imgHeader: '/img/projects/sesmt/sesmt-header-img.webp',
+    desktopImages: [
+      '/img/projects/sesmt/sesmt-dsk-1.webp',
+      '/img/projects/sesmt/sesmt-dsk-2.webp',
+    ],
+    mobileImages: [
+      '/img/projects/sesmt/sesmt-mobile-1.webp',
+      '/img/projects/sesmt/sesmt-mobile-2.webp',
+      '/img/projects/sesmt/sesmt-mobile-3.webp',
+      '/img/projects/sesmt/sesmt-mobile-4.webp',
+      '/img/projects/sesmt/sesmt-mobile-5.webp',
+      '/img/projects/sesmt/sesmt-mobile-6.webp',
+      '/img/projects/sesmt/sesmt-mobile-7.webp',
+    ]
+  },
+  {
+    thumbnail: '/img/projects/ampla-mais/ampla-home.webp',
+    title: 'Ampla +',
+    imgHeader: '/img/projects/ampla-mais/amplamais-header.webp',
+    desktopImages: [
+      '/img/projects/ampla-mais/ampla-dsk-1.webp',
+      '/img/projects/ampla-mais/ampla-dsk-2.webp',
+    ],
+    mobileImages: [
+      '/img/projects/ampla-mais/ampla-mobile-1.webp',
+      '/img/projects/ampla-mais/ampla-mobile-2.webp',
+      '/img/projects/ampla-mais/ampla-mobile-3.webp',
+      '/img/projects/ampla-mais/ampla-mobile-4.webp',
+      '/img/projects/ampla-mais/ampla-mobile-5.webp',
+      '/img/projects/ampla-mais/ampla-mobile-6.webp',
+      '/img/projects/ampla-mais/ampla-mobile-7.webp',
+      '/img/projects/ampla-mais/ampla-mobile-8.webp',
+      '/img/projects/ampla-mais/ampla-mobile-9.webp',
+    ]
+  },
+  {
+    thumbnail: '/img/projects/lh-homeopatia/lh-homeopatia-home.webp',
+    title: 'LH Homeopatia',
+    imgHeader: '/img/projects/lh-homeopatia/lh-homeopatia-header-img.webp',
+    desktopImages: [
+      '/img/projects/lh-homeopatia/lh-homeopatia-dsk-1.webp',
+      '/img/projects/lh-homeopatia/lh-homeopatia-dsk-2.webp',
+    ],
+    mobileImages: [
+      '/img/projects/lh-homeopatia/lh-homeopatia-mobile-1.webp',
+      '/img/projects/lh-homeopatia/lh-homeopatia-mobile-2.webp',
+      '/img/projects/lh-homeopatia/lh-homeopatia-mobile-3.webp',
+      '/img/projects/lh-homeopatia/lh-homeopatia-mobile-4.webp',
+      '/img/projects/lh-homeopatia/lh-homeopatia-mobile-5.webp',
+      '/img/projects/lh-homeopatia/lh-homeopatia-mobile-6.webp',
+    ]
+  },
+  {
+    thumbnail: '/img/projects/kelly/kelly-home.webp',
+    title: 'Kelly Niglio',
+    imgHeader: '/img/projects/kelly/kelly-header.webp',
+    desktopImages: [
+      '/img/projects/kelly/kelly-dsk-1.webp',
+      '/img/projects/kelly/kelly-dsk-2.webp',
+      '/img/projects/kelly/kelly-dsk-3.webp',
+      '/img/projects/kelly/kelly-dsk-4.webp',
+    ],
+    mobileImages: [
+      '/img/projects/kelly/kelly-mobile-1.webp',
+      '/img/projects/kelly/kelly-mobile-2.webp',
+      '/img/projects/kelly/kelly-mobile-3.webp',
+      '/img/projects/kelly/kelly-mobile-4.webp',
+      '/img/projects/kelly/kelly-mobile-5.webp',
+      '/img/projects/kelly/kelly-mobile-6.webp',
+      '/img/projects/kelly/kelly-mobile-7.webp',
+      '/img/projects/kelly/kelly-mobile-8.webp',
+      '/img/projects/kelly/kelly-mobile-9.webp',
+      '/img/projects/kelly/kelly-mobile-10.webp',
+      '/img/projects/kelly/kelly-mobile-11.webp',
+      '/img/projects/kelly/kelly-mobile-12.webp',
+      '/img/projects/kelly/kelly-mobile-13.webp',
+      '/img/projects/kelly/kelly-mobile-14.webp',
+    ]
+  },
+  {
+    thumbnail: '/img/projects/b4s-adv/b4s-adv-home.webp',
+    title: 'B4S Marketing Digital - Landing Page Advocacia',
+    imgHeader: '/img/projects/b4s-adv/b4s-adv-header-img.webp',
+    desktopImages: [
+      '/img/projects/b4s-adv/b4s-adv-dsk-1.webp',
+      '/img/projects/b4s-adv/b4s-adv-dsk-2.webp',
+    ],
+    mobileImages: [
+      '/img/projects/b4s-adv/b4s-adv-mobile-1.webp',
+      '/img/projects/b4s-adv/b4s-adv-mobile-2.webp',
+      '/img/projects/b4s-adv/b4s-adv-mobile-3.webp',
+      '/img/projects/b4s-adv/b4s-adv-mobile-4.webp',
+      '/img/projects/b4s-adv/b4s-adv-mobile-5.webp',
+      '/img/projects/b4s-adv/b4s-adv-mobile-6.webp',
+      '/img/projects/b4s-adv/b4s-adv-mobile-7.webp',
+    ]
+  },
+  {
+    thumbnail: '/img/projects/motion/motion-home.webp',
+    title: 'Motion Studio',
+    imgHeader: '/img/projects/motion/motion-header-img.webp',
+    desktopImages: [
+      '/img/projects/motion/motion-dsk-1.webp',
+      '/img/projects/motion/motion-dsk-2.webp',
+    ],
+    mobileImages: [
+      '/img/projects/motion/motion-mobile-1.webp',
+      '/img/projects/motion/motion-mobile-2.webp',
+      '/img/projects/motion/motion-mobile-3.webp',
+      '/img/projects/motion/motion-mobile-4.webp',
+      '/img/projects/motion/motion-mobile-5.webp',
+      '/img/projects/motion/motion-mobile-6.webp',
+    ]
+  },
+  {
+    thumbnail: '/img/projects/soho/soho-home.webp',
+    title: 'Soho Hair',
+    imgHeader: '/img/projects/soho/soho-header-img.webp',
+    desktopImages: [
+      '/img/projects/soho/soho-dsk-1.webp',
+      '/img/projects/soho/soho-dsk-2.webp',
+      '/img/projects/soho/soho-dsk-3.webp',
+    ],
+    mobileImages: [
+      '/img/projects/soho/soho-mobile-1.webp',
+      '/img/projects/soho/soho-mobile-2.webp',
+      '/img/projects/soho/soho-mobile-3.webp',
+      '/img/projects/soho/soho-mobile-4.webp',
+      '/img/projects/soho/soho-mobile-5.webp',
+      '/img/projects/soho/soho-mobile-6.webp',
+      '/img/projects/soho/soho-mobile-7.webp',
+    ]
+  },
+  {
+    thumbnail: '/img/projects/drcaramelo/drcaramelo-home.webp',
+    title: 'Dr. Caramelo',
+    imgHeader: '/img/projects/drcaramelo/drcaramelo-header-img.webp',
+    desktopImages: [
+      '/img/projects/drcaramelo/drcaramelo-dsk-1.webp',
+      '/img/projects/drcaramelo/drcaramelo-dsk-2.webp',
+    ],
+    mobileImages: [
+      '/img/projects/drcaramelo/drcaramelo-mobile-1.webp',
+      '/img/projects/drcaramelo/drcaramelo-mobile-2.webp',
+      '/img/projects/drcaramelo/drcaramelo-mobile-3.webp',
+      '/img/projects/drcaramelo/drcaramelo-mobile-4.webp',
+      '/img/projects/drcaramelo/drcaramelo-mobile-5.webp',
+      '/img/projects/drcaramelo/drcaramelo-mobile-6.webp',
+    ]
+  },
+  {
+    thumbnail: '/img/projects/b4s-solar/b4s-solar-home.webp',
+    title: 'B4S Marketing Digital - Landing Page Energia Solar',
+    imgHeader: '/img/projects/b4s-solar/b4s-solar-header-img.webp',
+    desktopImages: [
+      '/img/projects/b4s-solar/b4s-solar-dsk-1.webp',
+      '/img/projects/b4s-solar/b4s-solar-dsk-2.webp',
+    ],
+    mobileImages: [
+      '/img/projects/b4s-solar/b4s-solar-mobile-1.webp',
+      '/img/projects/b4s-solar/b4s-solar-mobile-2.webp',
+      '/img/projects/b4s-solar/b4s-solar-mobile-3.webp',
+      '/img/projects/b4s-solar/b4s-solar-mobile-4.webp',
+      '/img/projects/b4s-solar/b4s-solar-mobile-5.webp',
+      '/img/projects/b4s-solar/b4s-solar-mobile-6.webp',
+      '/img/projects/b4s-solar/b4s-solar-mobile-7.webp',
+      '/img/projects/b4s-solar/b4s-solar-mobile-8.webp',
+    ]
+  },
+];
