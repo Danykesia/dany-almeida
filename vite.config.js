fda0107f3d50ce7a8616a9c80cb7e9e53155fb7c
Daniele Késia <danielekesia@ComputaeDaniele.home>
2024-09-18 00:20:00 +0100

    create finbuddy page

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default {
         mainPt: resolve(__dirname, 'pt/index.html'),
         homeMate: resolve(__dirname, 'home-mate/index.html'),
         metalForEveryone: resolve(__dirname, 'metal-for-everyone/index.html'),
+        finBuddy: resolve(__dirname, 'fin-buddy/index.html'),
       }
     }
   }
